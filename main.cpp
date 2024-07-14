7a2699168af01e1887782d91a7381fe5147d7625
mikesvr6 <michaeloceallachain@gmail.com>
2024-07-14 11:41:32 +0000

    Step 2: setup program to respond to button presses, need store counts

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include "mbed.h"
+#include <cstdio>
 
-// main() runs in its own thread in the OS
+
+ InterruptIn BUTTON(PC_13); // Initialise the User button as the interupt button
+
+volatile int ispressed = 0;
+
+void BUTTON_pressed(){
+    ispressed = 1;
+}
 int main()
 {
-    while (true) {
-
-    }
+        
+        BUTTON.fall(&BUTTON_pressed);   //action to be taken when button is in fall (pushed)
+        
+        while (true) {
+            if (ispressed == 1) {
+                printf("Button press count %d \r\n", ispressed); //print to the command screen that the button has been pushed 
+                ispressed = 0;
+                
+            }
+        }
 }
-
